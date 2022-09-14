6137dd63f33efcf27ac73bf65e817a26d689d78b
fullStackZac <zac.childress.sr@gmail.com>
2022-09-13 20:33:44 -0500

    earning cpp

--- userInput/userInput/main.cpp
+++ userInput/userInput/main.cpp
@@ -7,8 +7,19 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    cout << "Enter your name: " << flush;
+    string input;
+    cin >> input;
+    cout << "You entered: " << input << endl;
+    
+    cout << "Enter a number: " << flush;
+    int value;
+    cin >> value;
+    cout << "You entered: " << value << endl;
+    
     return 0;
 }
